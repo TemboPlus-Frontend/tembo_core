3f21bd514bf40adc16fd0f61d0dd048be5748b3e
Okello <okellogeralddev@gmail.com>
2024-02-28 10:00:25 +0300

    updated locale constants

--- lib/constants/locale.dart
+++ lib/constants/locale.dart
@@ -1,40 +1,22 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:tembo_core/utils/date_utils.dart';
 
 enum TemboLocale {
-  en("en_US", "US", "English"),
-  sw("sw_TZ", "TZ", "Swahili"),
+  en("en", "US", "English"),
+  sw("sw", "TZ", "Swahili"),
   ;
 
-  const TemboLocale(this.languageCode, this.countryCode, this.name);
-  final String name;
+  const TemboLocale(this.languageCode, this.countryCode, this.label);
+  final String label;
   final String languageCode;
   final String countryCode;
 
-  Map<String, dynamic> toMap() {
-    return {
-      "name": name,
-      "languageCode": languageCode,
-      "countryCode": countryCode,
-    };
-  }
-
   Locale get locale => Locale(languageCode, countryCode);
 
-  String toJson() => jsonEncode(toMap());
+  String toJson() => languageCode;
 
-  static TemboLocale fromJson(String j) => TemboLocale.fromMap(jsonDecode(j));
-
-  static TemboLocale fromMap(Map<String, dynamic> map) {
-    final code = map["languageCode"];
-    if (code == null) throw "Unknown Locale";
-
-    final contained = ["en", "sw"].contains(code);
-    if (!contained) throw "Unknown Locale";
-
-    return TemboLocale.values.firstWhere((e) => e.languageCode == code);
+  static TemboLocale fromJson(String j) {
+    return TemboLocale.values.firstWhere((e) => e.languageCode == j);
   }
 
   @override
