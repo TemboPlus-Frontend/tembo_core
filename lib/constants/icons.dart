197921926e1c4e7df93ddf19367893c338b47f63
Okello <okellogeralddev@gmail.com>
2024-02-29 15:41:58 +0300

    updated icons

--- lib/constants/icons.dart
+++ lib/constants/icons.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:tembo_core/tembo_core.dart';
 
 class TemboIcon {
-  static IconData get homeOutline => isIOS ? CupertinoIcons.home : Icons.home_outlined;
+  static IconData get homeOutline =>
+      isIOS ? CupertinoIcons.home : Icons.home_outlined;
 
-  static IconData get shareOutline => isIOS ? CupertinoIcons.share : Icons.share_outlined;
+  static IconData get shareOutline =>
+      isIOS ? CupertinoIcons.share : Icons.share_outlined;
 
-  static IconData get personOutline => isIOS ? CupertinoIcons.person : Icons.person_outlined;
+  static IconData get personOutline =>
+      isIOS ? CupertinoIcons.person : Icons.person_outlined;
+
+  static IconData get arrowRightLeft =>
+      isIOS ? CupertinoIcons.arrow_right_arrow_left : Icons.swap_horiz_outlined;
+
+  static IconData get creditCardOutline =>
+      isIOS ? CupertinoIcons.creditcard : Icons.credit_card_outlined;
 }
